3312de3ef5afecae5db6cf55bc9798aec00cf516
Александр <medvedev.sasha2000@mail.ru>
2020-10-20 17:38:06 +0300

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from './auth';
+import { LoginComponent } from './login';
+import { RoutePageComponent } from './routes';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: RoutePageComponent, canActivate: [AuthGuard] },
+  { path: 'routes', component: RoutePageComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  // { path: 'routes/:id', component: FilmPageComponent }
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
