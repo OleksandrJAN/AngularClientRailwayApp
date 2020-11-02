b8c98d2626987a85ab16a9a3f3d2a3c3feb9eb00
Александр <medvedev.sasha2000@mail.ru>
2020-11-03 01:55:32 +0300

    Add resolver and auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AuthGuard } from './auth';
-import { LoginComponent } from './login';
-import { RoutePageComponent, RouteScheduleComponent, RoutesListComponent } from './routes';
+import { LoginComponent } from 'src/app/login';
+import { RoutePageComponent, RouteScheduleComponent, RoutesListComponent } from 'src/app/routes';
+import { AuthGuard, RouteScheduleResolver } from './service';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'routes', pathMatch: 'full' },
   {
-    path: 'routes', component: RoutePageComponent, canActivate: [AuthGuard],
+    path: 'routes', component: RoutePageComponent, canActivate: [AuthGuard], canActivateChild: [AuthGuard],
     children: [
       { path: '', component: RoutesListComponent },
-      { path: ':id', component: RouteScheduleComponent }
+      { path: ':id', component: RouteScheduleComponent, resolve: { routeSchedule: RouteScheduleResolver } }
     ]
   },
 
