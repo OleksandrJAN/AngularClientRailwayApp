64bc3d5ab859111c80913562a3bd20ab9cc05d8d
Александр <medvedev.sasha2000@mail.ru>
2020-11-02 23:34:13 +0300

    Fix export

--- src/app/service/index.ts
+++ src/app/service/index.ts
@@ -2,3 +2,4 @@ export * from './authentication.service';
 export * from './route.service';
 export * from './station.service';
 export * from './time-transformer.service';
+export * from './route-parameters.storage';
