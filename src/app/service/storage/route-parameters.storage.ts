efcac97a302fefa4db97d2409a4c8b56e524dea2
Александр <medvedev.sasha2000@mail.ru>
2020-11-03 20:47:38 +0300

    Store last route search parameters to session storage

--- src/app/service/storage/route-parameters.storage.ts
+++ src/app/service/storage/route-parameters.storage.ts
@@ -12,11 +12,19 @@ export class RouteParametersStorage {
     public routeParameters$: Observable<RouteSearchParameters>;
 
     constructor() {
-        this.routeParametersSubject = new BehaviorSubject<RouteSearchParameters>(null);
+        // get latest route parameters from session storage on page refresh
+        let routeParameters: RouteSearchParameters = JSON.parse(sessionStorage.getItem('latestRouteParameters'));
+        this.routeParametersSubject = new BehaviorSubject<RouteSearchParameters>(routeParameters);
         this.routeParameters$ = this.routeParametersSubject.asObservable();
     }
 
+    public get routeParametersValue(): RouteSearchParameters {
+        return this.routeParametersSubject.value;
+    }
+
     public updateRouteParameters(routeParameters: RouteSearchParameters) {
+        // store latest route search parameters in session storage to keep data between page refreshes for each browser tab
+        sessionStorage.setItem('latestRouteParameters', JSON.stringify(routeParameters));
         this.routeParametersSubject.next(routeParameters);
     }
 
