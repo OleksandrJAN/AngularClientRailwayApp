2522bb943b8308f729b2afe8bae45fd549854c53
Александр <medvedev.sasha2000@mail.ru>
2020-11-07 21:49:56 +0300

    Refactoring

--- src/app/routes/route-schedule/route-schedule.component.ts
+++ src/app/routes/route-schedule/route-schedule.component.ts
@@ -1,7 +1,7 @@
-import { AfterContentChecked, AfterViewChecked, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { TimeTransformer } from 'src/app/service';
+import { TimeTransformer } from 'src/app/_service';
 import { RouteSchedule, Schedule, Station } from 'src/app/_domain';
 
 
@@ -10,15 +10,15 @@ import { RouteSchedule, Schedule, Station } from 'src/app/_domain';
   templateUrl: './route-schedule.component.html',
   styleUrls: ['./route-schedule.component.css']
 })
-export class RouteScheduleComponent implements OnInit, AfterContentChecked, AfterViewChecked {
+export class RouteScheduleComponent implements OnInit {
 
   displayedColumns: string[] = ['station', 'arrival', 'departure', 'downtime', 'duration'];
 
   routeSchedule: RouteSchedule;
+
+  private departureTime: number;
   arrivalStationName: string;
   departureStationName: string;
-  travelTime: number = 0;
-
 
   constructor(
     private route: ActivatedRoute,
@@ -28,26 +28,17 @@ export class RouteScheduleComponent implements OnInit, AfterContentChecked, Afte
   ngOnInit(): void {
     // get route schedule from RouteScheduleResolver 
     this.routeSchedule = this.route.snapshot.data['routeSchedule'];
-    this.arrivalStationName = this.routeSchedule.stationsSchedule[0].station.name;
-    this.departureStationName = this.routeSchedule.stationsSchedule[this.routeSchedule.stationsSchedule.length - 1].station.name;
+    // init some component properties
+    this.initProperties();
   }
 
-  ngAfterContentChecked(): void {
-    // because of double rows checks in table, need to set shared variable travelTime default value
-    // first check is ngAfterContentChecked()
-    // second check is ngAfterViewChecked()
-    this.setDefaultTravelTime();
-  }
-
-  ngAfterViewChecked(): void {
-    // because of double rows checks in table, need to set shared variable travelTime default value
-    // first check is ngAfterContentChecked()
-    // second check is ngAfterViewChecked()
-    this.setDefaultTravelTime();
-  }
-
-  private setDefaultTravelTime(): void {
-    this.travelTime = 0;
+  private initProperties() {
+    // get departure train time
+    this.departureTime = this.getSchedule(0).departureTime;
+    // get first and last route stations
+    let stationsCount: number = this.routeSchedule.stationsSchedule.length;
+    this.arrivalStationName = this.getStation(0).name;
+    this.departureStationName = this.getStation(stationsCount - 1).name;
   }
 
   private getStation(index: number): Station {
@@ -80,13 +71,12 @@ export class RouteScheduleComponent implements OnInit, AfterContentChecked, Afte
       return TimeTransformer.LONG_DASH;
     }
     // schedule for row that are needed to calculate downtime
-    let stationSchedule: Schedule = this.routeSchedule.stationsSchedule[rowIndex].schedule;
-    let stationArrivalTime = stationSchedule.arrivalTime;
-    let stationDepartureTime = stationSchedule.departureTime;
+    let stationSchedule: Schedule = this.getSchedule(rowIndex);
+    let stationArrivalTime: number = stationSchedule.arrivalTime;
+    let stationDepartureTime: number = stationSchedule.departureTime;
 
-    let downtime = stationDepartureTime - stationArrivalTime;
+    let downtime: number = stationDepartureTime - stationArrivalTime;
     return this.timeTransformer.timeToString(downtime);
-    // return this.timeToString(downtime);
   }
 
   getTravelTime(rowIndex: number): string {
@@ -94,23 +84,11 @@ export class RouteScheduleComponent implements OnInit, AfterContentChecked, Afte
     if (rowIndex === 0) {
       return TimeTransformer.LONG_DASH;
     }
-    // schedule for current and previous rows that are needed to calculate travel time
-    let currStationSchedule: Schedule = this.routeSchedule.stationsSchedule[rowIndex].schedule;
-    let prevStationSchedule: Schedule = this.routeSchedule.stationsSchedule[rowIndex - 1].schedule;
 
-    let currStationArrivalTime = currStationSchedule.arrivalTime;
-    let prevStationArrivalTime = prevStationSchedule.arrivalTime;
-    let prevStationDepartureTime = prevStationSchedule.departureTime;
+    let stationArrivalTime: number = this.getSchedule(rowIndex).arrivalTime;
 
-    // Travel time between  two stations is equal to the difference between the arrival time to
-    //  first station (since there is a downtime of arrival and departure times to first station)
-    //  and the arrival time to second station.
-    // Then rowIndex === 1, previous station arrival time === null, so substract prevous station departure time,
-    //  in all other cases substract previous station arrival time
-    this.travelTime += currStationArrivalTime - (prevStationArrivalTime || prevStationDepartureTime);
-
-    return this.timeTransformer.timeToString(this.travelTime);
+    let travelTime: number = stationArrivalTime - this.departureTime;
+    return this.timeTransformer.timeToString(travelTime);
   }
-
 
 }
